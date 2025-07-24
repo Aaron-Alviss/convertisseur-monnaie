fd051b448e69027815bdb7c0bba8d0e3d0348eaa
Aaron-Alviss <aaronpaymer9@gmail.com>
2025-07-24 16:30:42 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// Taux de conversion
+const tauxCDF = 2800; // 1 USD = 2800 CDF
+
+// CDF -> USD
+function convertCDFtoUSD() {
+  const amount = parseFloat(document.getElementById('amountCDF').value);
+  if (isNaN(amount)) {
+    document.getElementById('resultCDFtoUSD').innerText = "Résultat : --";
+    return;
+  }
+  const result = (amount / tauxCDF).toFixed(2);
+  document.getElementById('resultCDFtoUSD').innerText = `Résultat : ${result} USD`;
+}
+
+// USD -> CDF
+function convertUSDtoCDF() {
+  const amount = parseFloat(document.getElementById('amountUSD').value);
+  if (isNaN(amount)) {
+    document.getElementById('resultUSDtoCDF').innerText = "Résultat : --";
+    return;
+  }
+  const result = (amount * tauxCDF).toFixed(2);
+  document.getElementById('resultUSDtoCDF').innerText = `Résultat : ${result} CDF`;
+}
